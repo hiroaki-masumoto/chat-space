fe938ae47537d35d3917fc029c4d7f027f2f2aec
hiroaki-masumoto <hiroaki1223@gmail.com>
2019-10-29 16:47:07 +0900

    incremental-edit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
-  var image = message.image ? `<img src=${message.image} >` : "";
   function buildHTML(message){
+    var image = message.image ? `<img src=${message.image} >` : "";
     var html =
       `<div class="message" data-message-id=${message.id}>
          <div class="upper-message">
@@ -19,28 +19,29 @@ $(function(){
          ${image}
        </div>`
     return html;
-   };
-});
-$('#new_message').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-    $('form')[0].reset();
-  })
-   .fail(function(){
-     alert('error');
+  };
+  $('#new_message').on('submit', function(e){
+   e.preventDefault();
+   var formData = new FormData(this);
+   var url = $(this).attr('action')
+   $.ajax({
+     url: url,
+     type: "POST",
+     data: formData,
+     dataType: 'json',
+     processData: false,
+     contentType: false
+   })
+    .done(function(data){
+      var html = buildHTML(data);
+      //alert('OK');
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+      $('form')[0].reset();
+    })
+     .fail(function(){
+       alert('error');
+     });
+     return false;
    });
-   return false;
- });
+});
